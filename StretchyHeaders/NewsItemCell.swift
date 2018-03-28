bdf6b00d3faa362d9cb68e18a28b6a55c8ecc81d
Nathan <nathan.hsu@icloud.com>
2018-03-27 17:27:43 -0700

    The whole thing lol

---
+++ StretchyHeaders/NewsItemCell.swift
@@ -0,0 +1,41 @@
+//
+//  NewsItemCell.swift
+//  StretchyHeaders
+//
+//  Created by Nathan Hsu on 2018-03-27.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class NewsItemCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+
+    var newsItem: NewsItem? {
+        didSet {
+            if let item = newsItem {
+                categoryLabel.text = item.category.toString()
+                categoryLabel.textColor = item.category.toColor()
+                summaryLabel.text = item.summary
+            }
+            else {
+                categoryLabel.text = nil
+                categoryLabel.text = nil
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ StretchyHeaders/TableViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  StretchyHeaders
+//
+//  Created by Nathan Hsu on 2018-03-27.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+import UIKit
+
+private let kTableHeaderHeight: CGFloat = 300.0
+
+class TableViewController: UITableViewController {
+
+    var headerView: UIView!
+    
+    let items = [
+        NewsItem(category: .World, summary: "Climate change protests, divestment meet fossil fuels realities"),
+        NewsItem(category: .Europe, summary: "Scotland's 'Yes' leader says independence vote is 'once in a lifetime'"),
+        NewsItem(category: .MiddleEast, summary: "Airstrikes boost Islamic State, FBI director warns more hostages possible"),
+        NewsItem(category: .Africa, summary: "Nigeria says 70 dead in building collapse; questions S. Africa victim claim"),
+        NewsItem(category: .AsiaPacific, summary: "Despite UN ruling, Japan seeks backing for whale hunting"),
+        NewsItem(category: .Americas, summary: "Officials: FBI is tracking 100 Americans who fought alongside IS in Syria"),
+        NewsItem(category: .World, summary: "South Africa in $40 billion deal for Russian nuclear reactors"),
+        NewsItem(category: .Europe, summary: "'One million babies' created by EU student exchanges")
+    
+    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        headerView = tableView.tableHeaderView
+        tableView.tableHeaderView = nil
+        tableView.addSubview(headerView)
+        tableView.contentInset = UIEdgeInsets(top: kTableHeaderHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
+        updateHeaderView()
+    }
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateHeaderView()
+    }
+
+    
+    // hides status bar.
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - functions
+    func updateHeaderView() {
+        var headerRect = CGRect(x: 0, y: -kTableHeaderHeight, width: tableView.bounds.width, height: kTableHeaderHeight)
+        if tableView.contentOffset.y < -kTableHeaderHeight {
+            headerRect.origin.y = tableView.contentOffset.y
+            headerRect.size.height = -tableView.contentOffset.y
+        }
+        headerView.frame = headerRect
+    }
+
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsItemCell
+        cell.newsItem = item
+        return cell
+    }
+
+}
+
